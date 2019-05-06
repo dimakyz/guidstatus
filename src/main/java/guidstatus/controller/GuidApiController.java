0656a7d59534c954c7450b307d34e0e3b88d8296
Dmitry Kuznetsov <dimakyz@mail.ru>
2019-05-06 15:04:10 +0300

    commit

--- src/main/java/guidstatus/controller/GuidApiController.java
+++ src/main/java/guidstatus/controller/GuidApiController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.math.BigInteger;
 
 @RestController
